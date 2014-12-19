7d150795ef30400e07177d91b4e4023ae4cecc1a
Nikhil Ballaney <nballaney@gmail.com>
2014-12-19 15:09:49 -0800

    (fix)

--- simulation.js
+++ simulation.js
@@ -3,8 +3,7 @@ var config = require('./config');
 
 exports.currentConsumption = function () {
   var currentTime = Date();
-  var slice = currentTime.slice;
-  var time = slice(16,18);  // 24 hour format
+  var time = currentTime.slice(16,18);  // 24 hour format
 
 // Simluating demand according to the time of the day
   if(time > 9 && time < 22){
