4f7f3ceddcfc1a2bf2bd02ec8d1af10376950a7f
Nikhil Ballaney <nballaney@gmail.com>
2014-12-19 17:04:45 -0800

    (feat) Simulation

--- simulation.js
+++ simulation.js
@@ -1,18 +1,25 @@
 // Made as per http://www.hydroone.com/RegulatoryAffairs/RatesPrices/Pages/ElectricityPrices.aspx
 var config = require('./config');
+var bid = config.midConsumption;
 
-exports.currentConsumption = function () {
-  var currentTime = Date();
-  var time = currentTime.slice(16,18);  // 24 hour format
-
+exports.bid = function (data) {
+  var time = data.time.slice(16,18);  // UTC date
 // Simluating demand according to the time of the day
-  if(time > 9 && time < 22){
-    if(time < 12 && time > 18) {
-      return config.maxConsumption - (Math.random() * config.deviation);
+  if(time > '9' && time < '22'){
+    if(time < '12' && time > '18') {
+      bid = config.maxConsumption - (Math.random() * config.bidDeviation);
+      return bid;
     } else {
-      return config.midConsumption + (Math.random() * config.deviation); 
+      bid = config.midConsumption + (Math.random() * config.bidDeviation); 
+      return bid;
     } 
   } else {
-    return config.midConsumption - (Math.random() * config.deviation);
+    bid = config.midConsumption - (Math.random() * config.bidDeviation);
+    return bid;
   }
+};
+
+
+exports.currentConsumption = function() {
+  return bid + (Math.random() * config.consumptionDeviation);
 }
