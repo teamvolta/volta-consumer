bef3233109e7e85232b6770d51d8d30370ecfcf7
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2014-12-19 21:08:07 -0800

    Dryer gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var install = require('gulp-install');
 var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
-  // default task will be added here
-  gulp.src('./package.json') //gulp.src fetches the file and passes it on as an argument
-  .pipe(install());
+   gulp.start('style', 'install');  // default task can be added here
 });
+
+gulp.task('install', function() {
+   gulp.src('./package.json') //gulp.src fetches the file and passes it on as an argument
+  .pipe(install());
+})
 
 gulp.task('test', function() {  //I am still not sure what it actually does
 	                            // passing shared module in all tests (according to docs)
