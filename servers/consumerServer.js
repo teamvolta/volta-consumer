c071f32beb5c6706a4e694d3da946e46e68c6bd9
Nikhil Ballaney <nballaney@gmail.com>
2015-01-03 16:23:50 -0800

    (feat) Time based simulation

--- servers/consumerServer.js
+++ servers/consumerServer.js
@@ -28,6 +28,7 @@ var demandSystem = 0;
 var allotedBySystem = 0;
 var allotedByBroker = 0;
 var currentProduction = 0;
+var simulationStartTime = Date.now();
 
 console.log('NODE_ENV', process.env.NODE_ENV); //to check whether it's been set to production when deployed
 
@@ -42,16 +43,15 @@ system.on('connect', function () {
 //   duration: ms
 // }
 system.on('startBidding', function (data) {
-  currentConsumption = simulation.currentConsumption();
   system.emit('bid', {
     // Better name for 'data' property
-    data: simulation.bid(data, demandSystem),
+    data: simulation.bid(data, demandSystem, simulationStartTime),
     consumerId: consumerId
   });
 });
 
 
-// System admin sends back the price for the time-slot
+// System admin sends back the price/energy for the time-slot
 system.on('receipt', function (receipt) {
  allotedBySystem = receipt.energy;
 });
@@ -59,11 +59,12 @@ system.on('receipt', function (receipt) {
 
 // System admin keeps track of total consumption of all consumers
 setInterval(function () {
+  currentConsumption = simulation.currentConsumption();
   system.emit('consume', {
     currentConsumption: currentConsumption,
     consumerId: consumerId
   });
-}, 100);
+}, 300);
 
 
 
@@ -72,14 +73,16 @@ broker.on('connect', function () {
   console.log('Connected to broker!');
 });
 
-broker.on('startCollection', function (data) {
+broker.on('startCollection', function (timeBlock) {
   if (demandBroker) {
     broker.emit('demand', {
+      timeBlock: timeBlock,
       energy: demandBroker,
       consumerId: consumerId
     });
   } else if (supplyBroker) {
     broker.emit('supply', {
+      timeBlock: timeBlock,
       energy: supplyBroker,
       consumerId: consumerId
     });
@@ -89,6 +92,7 @@ broker.on('startCollection', function (data) {
 
 // Accounting 
 account.on('connect', function () {
+  console.log('Connected to account!');
   socket.emit('buyer', consumerId);
   socket.emit('seller', consumerId);
 })
