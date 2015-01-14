c08f2004de03d7b9a5fa5c35ca31c052464983c1
Nikhil Ballaney <nballaney@gmail.com>
2015-01-13 21:35:13 -0800

    (fix) Remove console logs

--- utils/simulation.js
+++ utils/simulation.js
@@ -5,10 +5,7 @@ var simulation = function(config) {
   this.bidTime = Date.now();  
   this.consumption = config.min;
   this.expectedConsumption = this.consumption;
-  // console.log('--------' + config.mid);
 };
-
-// reporter.register('consumption', function(){return {bidTime: bidTime, consumption: consumption, bid: bid}});
 
 
 simulation.prototype.bid = function(data, demandSystem, startTime, min, max) {
@@ -17,36 +14,28 @@ simulation.prototype.bid = function(data, demandSystem, startTime, min, max) {
   var a = startTime;
   this.changedConsumption = true;
   var expected = this.consumption;
-  // console.log('-------- before for -------', expected);
   for(var i = 1000; i <= blockDuration; i+=1000) {
-    // console.log('-------- INSIDE for -------', expected)
     a -= 1000;
     expected = this.timeBasedChange(expected, a, min, max);
   }
-  // console.log('-------- after for -------', expected);
 
   this.expectedConsumption = expected;
-  // this.expectedConsumption = this.timeBasedChange(this.consumption, this.bidTime, min, max);
   var bids = [{
     price: this.config.bidPrice,
     energy: demandSystem
   }];
 
-  // reporter.report('bids', function(){return bids});
   return bids;
 };
 
 simulation.prototype.currentConsumption = function(startTime, min, max) {
   
-  // console.log('-------CURRENT-----', this.expectedConsumption);
   if(this.changedConsumption && Date.now() > this.bidTime) {
-  console.log('-------bidTime--------', new Date(this.bidTime));
     this.changedConsumption = false;
     this.consumption = this.expectedConsumption;
   } else {
     this.consumption = this.timeBasedChange(this.consumption, startTime, min, max);
   }
-  // console.log('INSIDE SIMULATION ' + this.consumption);
   return this.timeBasedChange(this.consumption, startTime, min, max);
 };
 
@@ -70,7 +59,6 @@ simulation.prototype.checkForMinMax = function(number, min, max) {
 };
 
 simulation.prototype.timeBasedChange = function(number, startTime, min, max) {
-  console.log('---------startTime------', new Date(startTime));
   var simulationTime = this.config.simulationTime;
   var timeElapsed = Date.now() - startTime;
   var stage = timeElapsed / simulationTime * 100;
