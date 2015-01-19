747ccce12a4cc0430750f62961fdfb909d86db5d
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-18 17:34:59 -0800

    Removed bower installation

--- deploy.sh
+++ deploy.sh
@@ -144,14 +144,14 @@ if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
 fi
 
 # 5. Install bower packages
-if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  # eval $NPM_CMD install bower
-  # exitWithMessageOnError "installing bower failed"
-  ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower failed"
-  cd - > /dev/null
-fi
+# if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   # eval $NPM_CMD install bower
+#   # exitWithMessageOnError "installing bower failed"
+#   ./node_modules/.bin/bower install
+#   exitWithMessageOnError "bower failed"
+#   cd - > /dev/null
+# fi
 
 
 # pushd "$DEPLOYMENT_TARGET"
