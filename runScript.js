41eac6c38223f231b8794a9ef9039f72d485239b
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-18 17:06:53 -0800

    Prepared consumer server for deployment

---
+++ runScript.js
@@ -0,0 +1,14 @@
+var spawn = require('child_process').spawn;
+
+if (process.env.SITE_TYPE === "backend") {
+  console.log("choosing backend run script");
+  spawn('node', ['consumer-server/server.js']);	
+} else if (process.env.SITE_TYPE === "producer") {
+  console.log("choosing producer run script");
+  spawn('node', ['producer-server/server.js']);
+} else if (process.env.SITE_TYPE === "frontend") {
+  console.log("choosing frontend run script");
+  spawn('node', ['client/client-server/server.js']);
+else {
+  console.log("SITE_TYPE variable should be set to backend, producer or frontend");
+}
