2471cab310e77187f086324f7aad552290384297
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-18 18:40:45 -0800

    Added diagnostics consolelogs

--- runScript.js
+++ runScript.js
@@ -2,13 +2,15 @@ var spawn = require('child_process').spawn;
 
 if (process.env.SITE_TYPE === "backend") {
   console.log("choosing backend run script");
-  
+  console.log(__dirname);
   spawn('node', ['consumer-server/server.js']);	
 } else if (process.env.SITE_TYPE === "producer") {
   console.log("choosing producer run script");
+  console.log(__dirname);
   spawn('node', ['producer-server/server.js']);
 } else if (process.env.SITE_TYPE === "frontend") {
   console.log("choosing frontend run script");
+  console.log(__dirname);
   spawn('node', ['client/client-server/server.js']);
 } else {
   console.log("SITE_TYPE variable should be set to backend, producer or frontend");
